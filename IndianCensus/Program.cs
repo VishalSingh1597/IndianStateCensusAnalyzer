45eb2419704f1f7c0c59fdbda38e4dbf90b18dc5
Vishal Singh <VishalSingh1597>
2021-05-20 18:55:40 +0530

    [Vishal]Added:Welcome Code

---
+++ IndianCensus/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianCensus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**************Welcome To Indian State Census Analyser******************");
+            Console.Read();
+        }
+    }
+}
